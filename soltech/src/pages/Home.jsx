c0ff0000490a730ab462117c5db4212e4a6cc6fe
Luka <luka.cerovic14@gmail.com>
2024-03-09 15:39:10 +0100

    fix the app

--- soltech/src/pages/Home.jsx
+++ soltech/src/pages/Home.jsx
@@ -102,7 +102,7 @@ export default function Home() {
     setReviewMessage(true);
     setActiveSection("");
     try {
-      const res = await fetch("/api/message/send", {
+      const res = await fetch("https://easy-pinafore-cow.cyclic.app/api/message/send", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
