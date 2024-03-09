0e88ae05c41430f006f156ca519b0095035fa372
Luka <luka.cerovic14@gmail.com>
2024-03-09 16:02:52 +0100

    fix the cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import messageRoute from './routes/message.route.js'
-
+import cors from 'cors';
 // Creating application:
 const app = express();
 app.use(express.json());
- 
+ app.use(cors({
+    origin:'*',
+ }))
 // kreiramo rikvestove
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
