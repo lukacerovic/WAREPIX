d1193bd161fd053c947cc52d9aa5b2ef03dd60d7
lukacerovic <124380132+lukacerovic@users.noreply.github.com>
2024-10-28 19:08:42 +0100

    Update MVP.jsx

--- soltech/src/pages/MVP.jsx
+++ soltech/src/pages/MVP.jsx
@@ -39,37 +39,38 @@ export default function MVP() {
         </div>
         <Experiance experiences={mvpServices}/>
       <div className='text-white flex flex-col' style={{background: 'transparent', paddingInline:'4vw'}}>
-        <h1 className='text-pink-500 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl' style={{paddingBottom:'2vw'}}>MVP (Minimum Viable Product)</h1>
-        <p className='text-[3vw] sm:text-xl md:text-2xl lg:text-2xl xl:text-2xl 2xl:text-3xl'>
-            Is a development strategy where a basic version of a product is released to gather maximum validated learning about customers with the least effort. It typically includes only core features, enough to deploy the product and test its concept in the market. MVP allows for early user feedback and validation, which informs further development and iterations.
-            <br/><br/>
-            At WAREPIX, we offer comprehensive understanding and assistance in developing MVP projects. We recognize that MVPs are the foundation of successful product launches, enabling businesses to validate their ideas and iterate based on user feedback. Our approach involves:
-            <br/><br/>
-            Thorough Analysis: We work closely with clients to understand their vision and goals for the MVP. Through detailed analysis, we identify core features essential for the initial release.The MVP is the smallest version of a product that can provide value to customers. It contains just
-        </p>
-        <div className='flex flex-col self-center' style={{gap:'1.5vw', paddingTop:'5vw', width:'80vw'}}>
-            <ol className='flex text-[2.3vw] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center' style={{gap:'1vw'}}>
-                <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>1</h1>
+        <h1 className='text-pink-500 text-[2.5rem] sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl' style={{paddingBottom:'2vw'}}>MVP (Minimum Viable Product)</h1>
+        <p className='text-[1.3rem] sm:text-xl md:text-2xl lg:text-2xl xl:text-2xl 2xl:text-3xl'>
+    A development approach where a basic product version is launched to quickly learn about customers with minimal effort. It includes only essential features to test the concept and gather feedback.
+    <br/><br/>
+    At WAREPIX, we specialize in building MVPs that validate ideas through user feedback. Our method focuses on:
+    <br/><br/>
+    Thorough Analysis: We work with clients to identify core features, creating an MVP that delivers value and sets a strong foundation for future iterations.
+</p>
+
+        <div className='flex flex-col self-center gap-[10vw] sm:gap-[1.5vw] w-[100%] sm:w-[80vw] pt-[10vw] sm:pt-[5vw]'>
+            <ol className='flex text-[1.2rem] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center gap-[5vw] sm:gap-[1vw]'>
+                <h1 className='text-[2rem] sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>1</h1>
                 Strategic Planning: Our team helps in planning and prioritizing features to ensure the MVP effectively addresses user needs while staying within budget and time constraints.
             </ol>
-            <ol className='flex text-[2.3vw] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center' style={{gap:'1vw'}}>
-               <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>2</h1>
+            <ol className='flex text-[1.2rem] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center gap-[5vw] sm:gap-[1vw]'>
+               <h1 className='text-[2rem] sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>2</h1>
                 Rapid Prototyping: We specialize in creating rapid prototypes and wireframes to visualize MVP functionalities and gather early feedback from stakeholders and potential users.
             </ol>
-            <ol className='flex text-[2.3vw] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center ' style={{gap:'1vw'}}>
-                <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>3</h1>
+            <ol className='flex text-[1.2rem] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center gap-[5vw] sm:gap-[1vw]'>
+                <h1 className='text-[2rem] sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>3</h1>
                 Agile Development: WAREPIX follows Agile methodologies to facilitate adaptive and iterative development cycles. This approach allows for flexibility and responsiveness to changes and feedback throughout the development process.
             </ol>
-            <ol className='flex text-[2.3vw] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center ' style={{gap:'1vw'}}>
-                <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>4</h1>
+            <ol className='flex text-[1.2rem] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center gap-[5vw] sm:gap-[1vw]'>
+                <h1 className='text-[2rem] sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>4</h1>
                 User-Centric Approach: We emphasize user testing and feedback integration throughout MVP development. By involving users early and often, we ensure that the MVP meets their expectations and addresses pain points effectively. 
             </ol>
-            <ol className='flex text-[2.3vw] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center ' style={{gap:'1vw'}}>
-                <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>5</h1>
+            <ol className='flex text-[1.2rem] sm:text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl items-center gap-[5vw] sm:gap-[1vw]'>
+                <h1 className='text-[2rem] sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl 2xl:text-6xl'>5</h1>
                 Continuous Improvement: After deployment, we continue to support the MVP, monitor its performance, and analyze user behavior. This data-driven approach helps in identifying areas for improvement and guiding future iterations.
             </ol>
         </div>
-        <p className='text-center text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl self-center' style={{width:'90vw', marginTop:'5vw'}}>
+        <p className='text-center text-xl sm:text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl self-center my-[10vw] sm:my-[5vw] w-[100%] sm:w-[90vw]'>
             Overall, WAREPIX's expertise and support in MVP development empower businesses to minimize risk, maximize learning, and accelerate their path to market success.  
         </p>
         <Link className='self-center' style={{marginTop:'5vw'}} to='https://www.warepix.com/#contact'>
